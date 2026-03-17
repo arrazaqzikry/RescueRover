2aa1781f7fed6d9592767c7d11c6228d36ea1d44
arrazaq zikry <zikryarrazaq@gmail.com>
2026-03-17 20:48:18 +0800

    first changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -10,6 +11,7 @@ export default defineConfig({
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
+
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
